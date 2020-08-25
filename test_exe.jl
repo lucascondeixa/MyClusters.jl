dbbb70093c2590ee90feb0908b398b078e298910
lucascondeixa <lucas.condeixa@aalto.fi>
2020-08-25 13:53:45 +0300

    Updates in the tests files

---
+++ test_exe.jl
@@ -0,0 +1,42 @@
+cd(@__DIR__)
+push!(LOAD_PATH, dirname(@__DIR__))
+
+include("myclusters_github.jl")
+include("test_func.jl")
+
+using DelimitedFiles
+
+## Test objects
+nodes = 3
+lines = [[1,3], [2,3]]
+Cluss = 1:650
+D = Array{Float64}(undef, 8760, nodes)
+W = Array{Float64}(undef, 8760, nodes)
+S = Array{Float64}(undef, 8760, nodes)
+max_load = Array{Float64}(undef, nodes)
+dem_inc = Array{Float64}(undef, nodes)
+m = zeros(8760)
+d = zeros(Int,8760)
+pD = 0.01                           # Percentile to find the critical points in Demand
+pWS = 0.99                          # Percentile to find the critical points in WS
+pace = 0.0005
+RP = 50
+pD_crit = 0.75
+pWS_crit = 0.25
+cases = ["SP", "DK", "DE"]
+
+## Exec...
+println("\nLoading cases...")
+@time load_cases()
+println("\nTesting kmeans...")
+@time test_kmeans()
+println("\nTesting HCnD_WD...")
+@time test_HCnD_WD()
+println("\nTesting HCnD_ED...")
+@time test_HCnD_ED()
+println("\nTesting crit_RP...")
+@time test_crit_RP()
+println("\nTesting find_crit_DWS...")
+@time test_find_crit_DWS()
+
+# End of Code
---
+++ test_func.jl
@@ -0,0 +1,85 @@
+include("myclusters_github.jl")
+using Test
+
+## Load instances
+function load_cases()
+    cd("instances")
+    for i in 1:nodes
+        case = cases[i]
+        df = CSV.read(string("Case_","$case","_New.csv"))
+        D[:,i] = Float64.(df.Load_mod)
+        W[:,i] = Float64.(df.Avail_Win)
+        S[:,i] = Float64.(df.Avail_Sol)
+        max_load[i] = Float64.(df.Max_Load[1])
+        dem_inc[i] = Float64.(df.Dem_Inc[1])
+        if i == 1
+            global m = Int64.(df.Month)
+        end
+    end
+    cd("..")
+    # Days accounting
+    for i in 1:8760
+        d[i] = Int(ceil(i/24))
+    end
+end
+
+## Test of kmeans over DWS with k=10
+function test_kmeans()
+    cd("examples")
+    a = kmeans3D(true,
+        10,
+        3,
+        D[:,1],
+        W[:,1],
+        S[:,1]
+    )
+    aux = readdlm("kmeans3D_10_first_s.txt")
+    if aux[:] != Int.(sort(a[3][10,1:10]))
+        println("\nDifferent seed, check code!")
+        return
+    end
+    # writedlm("wogrin3D_new_10.txt",a[1][1:10,:])
+    aux = readdlm("kmeans3D_10.txt")
+    cd("..")
+    @test a[1][1:10,:] == aux
+end
+
+## Test of HCnD using a reduced DWS[1:50,:]
+function test_HCnD_WD()
+    cd("examples")
+    a = HCnD(1,1,D[1:50,:],W[1:50,:],S[1:50,:])
+    # writedlm("HCnD_50.txt",a[1])
+    aux = readdlm("HCnD_WD_50.txt")
+    cd("..")
+    @test a[1] == aux
+end
+
+## Test of HCnD using a reduced DWS[1:50,:]
+function test_HCnD_ED()
+    cd("examples")
+    a = HCnD(1,2,D[1:50,:],W[1:50,:],S[1:50,:])
+    # writedlm("HCnD_ED_50.txt",a[1])
+    aux = readdlm("HCnD_ED_50.txt")
+    cd("..")
+    @test a[1] == aux
+end
+
+## Test of HCnD using a reduced DWS[1:50,:]
+function test_crit_RP()
+    cd("examples")
+    a = crit_RP(pD,pWS,D,W,S,nodes,RP,d,pace,max_load,dem_inc)
+    # writedlm("crit_RP_50.txt",a[8])
+    aux = readdlm("crit_RP_50.txt")
+    cd("..")
+    @test a[8] == aux[:]
+end
+
+## Test of HCnD using a reduced DWS[1:50,:]
+function test_find_crit_DWS()
+    cd("examples")
+    a = find_crit_DWS(pD_crit,pWS_crit,D,W,S,nodes)
+    # writedlm(string("find_crit_DWS_pD_",pD_crit,"_pWS_",pWS_crit,".txt"),a[3][3])
+    aux = readdlm(string("find_crit_DWS_pD_",pD_crit,"_pWS_",pWS_crit,".txt"))
+    cd("..")
+    @test a[3][3] == aux[:]
+end
