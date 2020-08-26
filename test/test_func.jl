148267b46301b7faef875e19ed8f085f0f69f945
Lucas <ldcondeixa@gmail.com>
2020-08-26 12:20:24 +0300

    Adapting to use test folder

--- test/test_func.jl
+++ test/test_func.jl
@@ -3,6 +3,7 @@ using Test
 
 ## Load instances
 function load_cases()
+    cd("..")
     cd("instances")
     for i in 1:nodes
         case = cases[i]
@@ -17,6 +18,7 @@ function load_cases()
         end
     end
     cd("..")
+    cd("test")
     # Days accounting
     for i in 1:8760
         d[i] = Int(ceil(i/24))
@@ -25,6 +27,7 @@ end
 
 ## Test of kmeans over DWS with k=10
 function test_kmeans()
+    cd("..")
     cd("examples")
     a = kmeans3D(true,
         10,
@@ -41,16 +44,19 @@ function test_kmeans()
     # writedlm("wogrin3D_new_10.txt",a[1][1:10,:])
     aux = readdlm("kmeans3D_10.txt")
     cd("..")
+    cd("test")
     @test a[1][1:10,:] == aux
 end
 
 ## Test of HCnD using a reduced DWS[1:50,:]
 function test_HCnD_WD()
+    cd("..")
     cd("examples")
     a = HCnD(1,1,D[1:50,:],W[1:50,:],S[1:50,:])
     # writedlm("HCnD_50.txt",a[1])
     aux = readdlm("HCnD_WD_50.txt")
     cd("..")
+    cd("test")
     @test a[1] == aux
 end
 
@@ -66,20 +72,24 @@ end
 
 ## Test of HCnD using a reduced DWS[1:50,:]
 function test_crit_RP()
+    cd("..")
     cd("examples")
     a = crit_RP(pD,pWS,D,W,S,nodes,RP,d,pace,max_load,dem_inc)
     # writedlm("crit_RP_50.txt",a[8])
     aux = readdlm("crit_RP_50.txt")
     cd("..")
+    cd("test")
     @test a[8] == aux[:]
 end
 
 ## Test of HCnD using a reduced DWS[1:50,:]
 function test_find_crit_DWS()
+    cd("..")
     cd("examples")
     a = find_crit_DWS(pD_crit,pWS_crit,D,W,S,nodes)
     # writedlm(string("find_crit_DWS_pD_",pD_crit,"_pWS_",pWS_crit,".txt"),a[3][3])
     aux = readdlm(string("find_crit_DWS_pD_",pD_crit,"_pWS_",pWS_crit,".txt"))
     cd("..")
+    cd("test")
     @test a[3][3] == aux[:]
 end
